11b34882c4d00e7daaf75de5f8807e4277de73bb
Zack Yang <zack9433@gmail.com>
2016-09-23 10:36:12 +0800

    chore(build): modify for webpack 2 breaking changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const path = require('path');
 const webpack = require('webpack');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
@@ -13,27 +11,21 @@ const config = {
     filename: 'sanji-navbar-ui.js'
   },
   resolve: {
-    root: [nodeRoot],
-    // npm-linked packages can locate missing dependencies in app's node_modules
-    fallback: nodeRoot,
     alias: {
       'angular-material.css': nodeRoot + '/angular-material/angular-material.css',
       'angular-material-icons.css': nodeRoot + '/angular-material-icons/angular-material-icons.css',
       'angular-material-data-table.css': nodeRoot + '/angular-material-data-table/dist/md-data-table.css',
       'toastr.css': nodeRoot + '/toastr/build/toastr.css'
     },
-    extensions: ['', '.js', '.json', 'html', 'scss', 'css']
+    extensions: ['.js', '.json', 'html', 'scss', 'css']
   },
   module: {
-    preLoaders: [
-      {test: /\.js$/, loader: 'eslint', exclude: /(node_modules)/}
-    ],
-    loaders: [
+    rules: [
+      { test: /\.js$/, loader: 'eslint', exclude: /node_modules/, enforce: 'pre' },
       { test: /\.js$/, loader: 'babel?cacheDirectory', exclude: /node_modules/ },
       { test: /\.json$/, loader: 'json', exclude: /node_modules/ },
       { test: /\.html$/, loader: 'ng-cache?prefix=[dir]/[dir]', exclude: [/node_modules/, path.join(__dirname, '/app/index.html')] }
-    ],
-    noParse: []
+    ]
   },
   plugins: [
     new ProgressBarPlugin(),
@@ -41,8 +33,7 @@ const config = {
       __TEST__: 'test' === NODE_ENV,
       __DEV__: 'development' === NODE_ENV,
       __RELEASE__: 'production' === NODE_ENV
-    }),
-    new webpack.NoErrorsPlugin()
+    })
   ]
 };
 
