adf27244f9e93050d833422ff768861828236b05
Zack Yang <zack9433@gmail.com>
2017-02-13 10:54:26 +0800

    feat(build): modify extract setting

--- webpack.build.js
+++ webpack.build.js
@@ -12,33 +12,28 @@ config.entry = {
 config.output.filename = 'sanji-navbar-ui.js';
 config.output.libraryTarget = 'umd';
 config.output.library = 'sjNavbar';
-config.externals = [
-  'angular',
-  'sanji-core-ui'
-];
+config.externals = ['angular', 'sanji-core-ui'];
 
 config.module.rules = [
-  {test: /\.js$/, use: 'ng-annotate-loader', exclude: /(node_modules)/, enforce: 'post'},
+  { test: /\.js$/, use: 'ng-annotate-loader', exclude: /(node_modules)/, enforce: 'post' },
   {
     test: /\.scss$/,
     loader: ExtractTextPlugin.extract({
-      notExtractLoader: 'style-loader',
-      loader: 'css-loader!postcss-loader!sass-loader?includePaths[]=' + bourbon
+      fallback: 'style-loader',
+      use: 'css-loader!postcss-loader!sass-loader?includePaths[]=' + bourbon
     })
   }
 ].concat(config.module.rules);
 
 config.plugins.push(
   new ExtractTextPlugin('sanji-navbar-ui.css'),
-  new LodashModuleReplacementPlugin,
+  new LodashModuleReplacementPlugin(),
   new webpack.LoaderOptionsPlugin({
     minimize: true,
     debug: false,
     quiet: true,
-    options:{
-      postcss: [
-        autoprefixer({ browsers: ['last 2 versions'] })
-      ]
+    options: {
+      postcss: [autoprefixer({ browsers: ['last 2 versions'] })]
     }
   }),
   new webpack.optimize.UglifyJsPlugin({
